bef00abeaddf7a22212d88e2f6726a1d0190b760
Avery Terrel <averymt@proton.me>
2025-04-22 19:13:49 -0500

    Fix bugs in endswith

--- endswith.c
+++ endswith.c
@@ -4,8 +4,10 @@
 #include <string.h>
 
 bool endswith(const char * a, const char * b) {
+	if (a == NULL || b == NULL) return false;
 	size_t as = strlen(a);
 	size_t bs = strlen(b);
-	if (as <= bs) return false;
+	if (as == 0 && bs == 0) return true;
+	if (as < bs) return false;
 	return memcmp(a + as - bs, b, bs) == 0;
 }

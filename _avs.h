9c33114089e7e63274b6c4e659cfc4f255d0627e
Avery Terrel <averymt@proton.me>
2025-05-30 15:06:49 -0500

    Clean up _avs.h

--- _avs.h
+++ _avs.h
@@ -9,6 +9,7 @@
 #endif
 
 #define STREQ(a, b) ((a != NULL && b != NULL) && strcmp(a, b) == 0)
+
 #if defined(AVS_RELEASE) || defined(AVS_NO_UNUSED_VAR)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-variable"

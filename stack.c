f343ab302fc32357e3707ffc92bf1c92f6a9807c
Avery Terrel <averymt@proton.me>
2025-05-30 15:07:11 -0500

    Rework stack.c entirely

--- stack.c
+++ stack.c
@@ -8,57 +8,60 @@
 
 avs_stack_t* avs_stack_new() {
 	errno = 0;
-	avs_stack_t* stack = calloc(1, sizeof(avs_stack_t));
+	avs_stack_t* stack = malloc(sizeof(avs_stack_t));
 	if (!stack) {
-		avs_debug("New stack allocation failed\n");
+		if (errno == 0) errno = ENOMEM;
 		return NULL;
 	}
+	stack->sz = 0;
+	stack->itms = NULL;
 	return stack;
 }
 
-#if defined(AVS_RELEASE) || defined(AVS_NO_UNUSED_VAR)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wanalyzer-malloc-leak"
-#endif
-// realloc is incorrectly marked as a leak
 void* avs_stack_push(avs_stack_t* stack, void* ptr) {
 	errno = 0;
-	void** itms = realloc(stack->itms, sizeof(void*)*(stack->sz+1));
-	if (!itms) {
-		avs_debug("Stack reallocation during push failed\n");
+	if (!stack) {
+		errno = EINVAL;
 		return NULL;
 	}
-	stack->itms = itms;
-	stack->itms[stack->sz] = ptr;
+	void** itms = realloc(stack->itms, sizeof(void*) * (stack->sz+1));
+	if (!itms) {
+		if (errno == 0) errno = ENOMEM;
+		return NULL;
+	}
 	stack->sz++;
+	stack->itms = itms;
 	return ptr;
 }
 
 void* avs_stack_pop(avs_stack_t* stack) {
 	errno = 0;
-	if (stack->sz == 0) {
-		avs_debug("Stack contains no more items\n");
+	if (!stack) {
 		errno = EINVAL;
 		return NULL;
 	}
-	void* itm = stack->itms[stack->sz];
-	void** itms = realloc(stack->itms, sizeof(void*)*(stack->sz-1));
-	if (!itms) {
-		avs_debug("Stack reallocation during pop failed\n");
+	if (stack->sz == 0) {
+		errno = ERANGE;
 		return NULL;
 	}
-	stack->itms = itms;
+	void* ptr = stack->itms[stack->sz-1];
+	void** itms = realloc(stack->itms, sizeof(void*) * (stack->sz-1));
+	if (!itms) {
+		if (errno == 0) errno = ENOMEM;
+		return NULL;
+	}
 	stack->sz--;
-	return itm;
+	stack->itms = itms;
+	return ptr;
 }
-#if defined(AVS_RELEASE) || defined(AVS_NO_UNUSED_VAR)
-#pragma GCC diagnostic pop
-#endif
 
 void* avs_stack_peek(avs_stack_t* stack, size_t index) {
 	errno = 0;
-	if (index > stack->sz) {
-		avs_debug("Peek index during larger than stack size\n");
+	if (!stack) {
+		errno = EINVAL;
+		return NULL;
+	}
+	if (stack->sz < index+1) {
 		errno = EINVAL;
 		return NULL;
 	}
@@ -66,12 +69,11 @@ void* avs_stack_peek(avs_stack_t* stack, size_t index) {
 }
 
 int avs_stack_free(avs_stack_t* stack) {
+	errno = 0;
 	if (!stack) {
-		avs_debug("Tried to free null stack\n");
-		return 0;
+		errno = EINVAL;
+		return -1;
 	}
-	if (stack->itms) free(stack->itms);
-	else avs_debug("Tried to free manually-freed stack items\n");
 	free(stack);
 	return 0;
 }
